66e620c2649b0ce7c1dc465b8f249fba8eafc4f5
Zachary Mowatt <zamowatt@gmail.com>
2023-05-04 15:03:21 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return 'This Saturday, I want to ' + activity + '!'
+}
+
+saturdayFun("go boxing")
+
+function mondayWork(task = "go to the office"){
+    return 'This Monday, I will ' + task + '.'
+}
+
+mondayWork("call in sick")
+
+function wrapAdjective(visualFlair = '*') {
+    return function(descriptor = "special") {
+        return `You are ${visualFlair}${descriptor}${visualFlair}!`
+    }
+}
+
+wrapAdjective("||")("amazing")
+
